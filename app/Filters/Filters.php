9dfd504ff79757fab8e3639b51cb7b4eb67cead1
Cameron Adams <cjadams13@users.noreply.github.com>
2018-04-26 21:12:37 -0600

    Refactoring madness.

---
+++ app/Filters/Filters.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kaiden
+ * Date: 4/26/18
+ * Time: 8:30 PM
+ */
+
+namespace App\Filters;
+
+
+use Illuminate\Http\Request;
+
+abstract class Filters
+{
+    /**
+     * @var Request
+     */
+    protected $request, $builder;
+
+    protected $filters = [];
+
+    /**
+     * ThreadsFilter constructor.
+     * @param Request $request
+     */
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    /**
+     * @param $builder
+     * @return mixed
+     */
+    public function apply($builder)
+    {
+        $this->builder = $builder;
+
+        foreach ($this->getFilters() as $filter => $value) {
+            if (method_exists($this, $filter)) {
+                $this->$filter($value);
+            }
+        }
+
+        return $this->builder;
+    }
+
+    public function getFilters()
+    {
+        return $this->request->only($this->filters);
+    }
+}
